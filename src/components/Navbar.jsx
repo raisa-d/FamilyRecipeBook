6e2c32c74605d414d2380a79485c9f421f6e7685
Raisa Dorzback <raisa.dorzback@gmail.com>
2024-10-17 16:45:43 -0400

    minor styling of nav links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,17 +4,17 @@ const Navbar = () => {
   return (
     <nav className='bg-lime-950 p-4'>
         <div className='flex container mx-auto justify-between'>
-            <li className='list-none text-white'>
-                <a href="/">Family Recipe Book</a>
+            <li className='list-none text-white font-bold'>
+                <a className="py-2 px-3 rounded-3xl border-white-100 hover:bg-white hover:text-black" href="/">Family Recipe Book</a>
             </li>
-            <li className='list-none text-white'>
-                <a href="/">All Recipes</a>
+            <li className='list-none text-white font-bold'>
+                <a className="py-2 px-3 rounded-3xl border-white-100 hover:bg-white hover:text-black" href="/">All Recipes</a>
             </li>
-            <li className='list-none text-white'>
-                <a href="/">Add Recipe</a>
+            <li className='list-none text-white font-bold'>
+                <a className="py-2 px-3 rounded-3xl border-white-100 hover:bg-white hover:text-black" href="/">Add Recipe</a>
             </li>
-            <li className='list-none text-white'>
-                <a href="/">About</a>
+            <li className='list-none text-white font-bold'>
+                <a className="py-2 px-3 rounded-3xl border-white-100 hover:bg-white hover:text-black" href="/">About</a>
             </li>
         </div>
     </nav>
